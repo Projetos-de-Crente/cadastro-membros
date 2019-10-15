150bd0cd5f575e2e82b36ca28f8dee96b73f2cab
Matheus <matheus.manhaes@ed.company>
2019-10-15 15:55:34 -0300

    Adiciona serviço de CRUD e modelo de membro

---
+++ src/app/membros/shared/membro.model.ts
@@ -0,0 +1,22 @@
+export class Membro {
+  constructor(
+    public id?: number,
+    public nome?: string,
+    public sexo?: string,
+    public dataNascimento?: string,
+    public idade?: number,
+    public cpf?: string,
+    public profissao?: string,
+    public email?: string,
+    public telefone?: string,
+    public celular?: string,
+    public logradouro?: string,
+    public numero?: number,
+    public bairro?: string,
+    public estado?: string,
+    public cep?: string,
+    public cidade?: string,
+    public complemento?: string,
+    public ativo?: boolean
+  ) {}
+}
---
+++ src/app/membros/shared/membro.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@angular/core';
+import { Membro } from './membro.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MembroService {
+
+  constructor() { }
+  listarMembros(): Membro[] {
+    // tslint:disable-next-line: no-string-literal
+    const membros = localStorage['membros'];
+    return membros ? JSON.parse(membros) : [];
+  }
+
+  cadastrarMembro(membro: Membro): void {
+    const membros = this.listarMembros();
+    // getTime() retorna os milissegundos passados entre 1 de Janeiro de 1970 até a data atual.
+    membro.id = new Date().getTime();
+    membros.push(membro);
+    // tslint:disable-next-line: no-string-literal
+    localStorage['membros'] = JSON.stringify(membros);
+  }
+
+  buscarMembroPorId(id: number): Membro {
+    const membros: Membro[] = this.listarMembros();
+    return membros.find(membro => membro.id === id);
+  }
+
+  atualizarMembro(membro: Membro): void {
+    const membros: Membro[] = this.listarMembros();
+    membros.forEach((obj, index, objs) => {
+      if (membro.id === obj.id) {
+        objs[index] = membro;
+      }
+    });
+    // tslint:disable-next-line: no-string-literal
+    localStorage['membro'] = JSON.stringify(membros);
+  }
+
+  removerMembro(id: number): void {
+    let membros: Membro[] = this.listarMembros();
+    membros = membros.filter(membro => membro.id !== id);
+    // tslint:disable-next-line: no-string-literal
+    localStorage['membros'] = JSON.stringify(membros);
+  }
+
+  alterarStatusMembro(id: number): void {
+    const membros: Membro[] = this.listarMembros();
+    membros.forEach((obj, index, objs) => {
+      if (id === obj.id) {
+        objs[index].ativo = !obj.ativo;
+      }
+    });
+    // tslint:disable-next-line: no-string-literal
+    localStorage['membros'] = JSON.stringify(membros);
+  }
+
+}
