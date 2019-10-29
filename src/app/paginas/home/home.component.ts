53e594cfde66dc0bf098233bf9c86168880beae6
Matheus <matheus.manhaes@ed.company>
2019-10-29 17:56:48 -0300

    Adiciona particles

--- src/app/paginas/home/home.component.ts
+++ src/app/paginas/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var particlesJS: any;
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,11 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    particlesJS.load('particles-js', 'assets/particles.json');
   }
-
 }
+
+
+
+
+
