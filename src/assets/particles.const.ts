32ad8280b6f5bbbe7dd3c5a203763ec87667ff32
Matheus <matheus.manhaes@ed.company>
2019-10-30 11:34:53 -0300

    Atualiza particles

--- src/assets/particles.const.ts
+++ src/assets/particles.const.ts
@@ -2,7 +2,7 @@ export const TEMA = {
     position: 'fixed',
     width: '100%',
     height: '100%',
-    'z-index': 1,
+    'z-index': 0,
     top: 0,
     left: 0,
     right: 0,
@@ -10,69 +10,107 @@ export const TEMA = {
 };
 
 export const PARAMS = {
-    number: {
-        value: 125,
-        density: {
+    particles: {
+        number: {
+          value: 125,
+          density: {
+            enable: true,
+            value_area: 962.0472365193136
+          }
+        },
+        color: {
+          value: '#646464'
+        },
+        shape: {
+          type: 'circle',
+          stroke: {
+            width: 0,
+            color: '#000000'
+          },
+          polygon: {
+            nb_sides: 5
+          },
+        },
+        opacity: {
+          value: 0.37680183430339786,
+          random: false,
+          anim: {
+            enable: false,
+            speed: 1,
+            opacity_min: 0.1,
+            sync: false
+          }
+        },
+        size: {
+          value: 3,
+          random: true,
+          anim: {
+            enable: false,
+            speed: 40,
+            size_min: 0.1,
+            sync: false
+          }
+        },
+        line_linked: {
           enable: true,
-          value_area: 962.0472365193136
-        }
-      },
-      color: {
-        value: '#646464'
-      },
-      shape: {
-        type: 'circle',
-        stroke: {
-          width: 0,
-          color: '#000000'
+          distance: 150,
+          color: '#646464',
+          opacity: 0.4,
+          width: 1
         },
-        polygon: {
-          nb_sides: 5
+        move: {
+          enable: true,
+          speed: 6,
+          direction: 'none',
+          random: false,
+          straight: false,
+          out_mode: 'out',
+          bounce: false,
+          attract: {
+            enable: false,
+            rotateX: 600,
+            rotateY: 1200
+          }
+        }
+      },
+      interactivity: {
+        detect_on: 'canvas',
+        events: {
+          onhover: {
+            enable: false,
+            mode: 'grab'
+          },
+          onclick: {
+            enable: true,
+            mode: 'push'
+          },
+          resize: true
         },
-        image: {
-          width: 100,
-          height: 100
+        modes: {
+          grab: {
+            distance: 400,
+            line_linked: {
+              opacity: 1
+            }
+          },
+          bubble: {
+            distance: 400,
+            size: 40,
+            duration: 2,
+            opacity: 8,
+            speed: 3
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4
+          },
+          push: {
+            particles_nb: 4
+          },
+          remove: {
+            particles_nb: 2
+          }
         }
       },
-      opacity: {
-        value: 0.37680183430339786,
-        random: false,
-        anim: {
-          enable: false,
-          speed: 1,
-          opacity_min: 0.1,
-          sync: false
-        }
-      },
-      size: {
-        value: 3,
-        random: true,
-        anim: {
-          enable: false,
-          speed: 40,
-          size_min: 0.1,
-          sync: false
-        }
-      },
-      line_linked: {
-        enable: true,
-        distance: 150,
-        color: '#646464',
-        opacity: 0.4,
-        width: 1
-      },
-      move: {
-        enable: true,
-        speed: 6,
-        direction: 'none',
-        random: false,
-        straight: false,
-        out_mode: 'out',
-        bounce: false,
-        attract: {
-          enable: false,
-          rotateX: 600,
-          rotateY: 1200
-        }
-      }
+      retina_detect: true
 };
